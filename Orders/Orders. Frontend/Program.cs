20510bc31bad7d9dbf32b27da34749eeb29500a1
David <davidtorresn@gmail.com>
2025-01-09 18:11:20 -0300

    add countri controller

--- Orders/Orders. Frontend/Program.cs
+++ Orders/Orders. Frontend/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Orders._Frontend;
+using Orders.Frontend;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
