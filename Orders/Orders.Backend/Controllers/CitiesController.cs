3340fc90867825b23c635d721eec81dbd0b0c85e
David <davidtorresn@gmail.com>
2025-01-24 12:24:24 -0300

    JWT

--- Orders/Orders.Backend/Controllers/CitiesController.cs
+++ Orders/Orders.Backend/Controllers/CitiesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Orders.Backend.UnitsOfWork.Implementations;
 using Orders.Backend.UnitsOfWork.Interfaces;
 using Orders.Shared.DTOs;
@@ -8,6 +10,7 @@ using System;
 namespace Orders.Backend.Controllers
 {
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
     public class CitiesController : GenericController<City>
     {
